41935130698d25249b2050036edea4f655317b90
Vital9i <vitalikdoiniak@gmail.com>
2024-09-23 23:48:44 +0300

    main complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import styled from 'styled-components';
 import { Header } from './layout/header/Header';
+import { Main } from './layout/sections/main/Main';
 
 
 
@@ -8,6 +9,7 @@ function App() {
     return (
         <div className="App">
             <Header />
+            <Main/>
         </div>
     );
 }
---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import photo from '../../../assets/images/photo.jpg'
+import { FlexWrapper } from "../../../components/FlexWrapper";
+
+export const Main = () => {
+    return (
+        <StyledMain>
+            <FlexWrapper aling={'center'} justify={'space-around'}>
+
+                <div>
+                    <span>Hi There</span>
+                    <Name>I am Vitali Doiniak</Name>
+                    <MainTitle>A Web Developer.</MainTitle>
+                </div>
+
+                <Photo src={photo} alt='' />
+
+            </FlexWrapper >
+        </StyledMain >
+    )
+}
+
+const StyledMain = styled.div`
+    min-height: 100vh;
+    background-color: #fff5e7;
+`
+
+const Photo = styled.img`
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+`
+const MainTitle = styled.h1 `
+
+`
+const Name = styled.span`
+    
+`
