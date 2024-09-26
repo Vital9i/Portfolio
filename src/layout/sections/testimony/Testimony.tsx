f9525f92afb2c80b8607db007430c4d84c9112cc
Vital9i <vitalikdoiniak@gmail.com>
2024-09-26 13:38:01 +0300

    TestimonySectionComplete

---
+++ src/layout/sections/testimony/Testimony.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import { SectionTitle } from '../../../components/SectionTitle';
+import { Icon } from '../../../components/icon/Icon';
+import { Slider } from '../../../components/slider/Slider';
+import { FlexWrapper } from '../../../components/FlexWrapper';
+
+export const Testimony = () => {
+    return (
+        <StyledTestimony>
+            <SectionTitle>Testimony</SectionTitle>
+            <FlexWrapper direction={'column'} aling={'center'}>
+                <Icon iconId={'testimony'} />
+                <Slider />
+            </FlexWrapper>
+        </StyledTestimony>
+    );
+};
+
+const StyledTestimony = styled.section`
+    min-height: 100vh;
+    background-color: #8093f6;
+`
+
