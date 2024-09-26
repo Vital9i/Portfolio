4b3a9ea9dbf90ecedca59ef03b7fe3022393d8ea
Vital9i <vitalikdoiniak@gmail.com>
2024-09-27 00:02:29 +0300

    CssHeaderComplete

--- src/layout/header/headerMenu/HeaderMenu.tsx
+++ src/layout/header/headerMenu/HeaderMenu.tsx
@@ -31,16 +31,14 @@ const StyledHeaderMenu = styled.nav`
         justify-content: center;
     }
 `
-const ListItem = styled.li` 
-position: relative;
-`
+
 const Link = styled.a` 
 font-family: Josefin Sans, sans-serif;
 font-size: 30px;
 font-weight: 400;
 line-height: 55px;
 text-align: center;
-
+color: transparent;
 `
 const Mask = styled.span`
     position: absolute;
@@ -49,8 +47,8 @@ const Mask = styled.span`
     display:inline-block;
     height:50%;
     overflow-y: hidden;
-    outline: 1px solid red;
-    color: ${theme.colors.accent}
+    /* outline: 1px solid red; */
+    color: ${theme.colors.accent};
 
     & + & {
         top:50%;
@@ -59,5 +57,39 @@ const Mask = styled.span`
             transform: translateY(-50%);
         }
     }
-
+    
 `
+const ListItem = styled.li`
+position: relative;
+
+&::before {
+    content: '';
+    display: inline-block;
+    height: 3px;
+    background-color:${theme.colors.accent};
+
+    position:absolute;
+    top:50%;
+    left: -10px;
+    right:-10px;
+    z-index: 1;
+
+    transform: scale(0);
+}
+
+&:hover {
+    &::before {
+        transform: scale(1);
+    }
+
+    ${Mask} {
+transform: skewX(12deg) translateX(5px) ;
+color:${theme.colors.font};
+
+& + ${Mask} {
+        transform: skewX(12deg) translateX(-5px) ;
+    }
+    }
+   }
+`
+
