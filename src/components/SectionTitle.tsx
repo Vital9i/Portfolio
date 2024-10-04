0eb573b8950d17dabf728905b11683695faa3f02
Vital9i <vitalikdoiniak@gmail.com>
2024-10-04 14:34:37 +0300

    @madiaSkills

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import { theme } from "../styles/Theme";
+import { font } from "../styles/Common";
 
 export const SectionTitle = styled.h2`
-font-family: Josefin Sans,sans-serif;
-font-size: 36px;
-font-weight: 600;
-letter-spacing: 5px;
+${font({family:'"Josefin Sans",sans-serif',weight: 600,Fmax:36,Fmin:30})}
 text-align:center;
+letter-spacing: 5px;
 margin-bottom: 90px;
 
 position: relative;
@@ -23,5 +22,9 @@ position: relative;
   bottom: -30px;
   left: 50%;
   transform: translateX(-50%);
+
+  @media ${theme.media.mobile}{
+    bottom: -24px;
+}
 }
   `
